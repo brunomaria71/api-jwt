d88821699e0640de53e7a99e25bcdf932809029d
brunomaria71 <brunomaria71@yahoo.com>
2022-06-07 14:46:58 -0400

    changed pw to hashpw

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,21 @@ import mySecretKey from "./secret.js";
 
 const users = [
   // fake database
-  { id: 1, email: "mara@bocacode.com", password: "123abc" },
-  { id: 2, email: "chole@bocacode.com", password: "def123" },
-  { id: 3, email: "rodrigo@bocacode.com", password: "123jkl" },
+  {
+    id: 1,
+    email: "mara@bocacode.com",
+    password: "$2b$10$WwhgVbFf9AJm5WWwpKVMYuUK0jVjOe0zb.Xv43oGbEfpBQ9ZXZp6a",
+  },
+  {
+    id: 2,
+    email: "chole@bocacode.com",
+    password: "$2b$10$WwhgVbFf9AJm5WWwpKVMYu0TL7APRFPuLos3bWhGajj8cI044uZnG",
+  },
+  {
+    id: 3,
+    email: "rodrigo@bocacode.com",
+    password: "$2b$10$WwhgVbFf9AJm5WWwpKVMYuHlJ1vM/oVfHBCwopiUiXp4TeUoXzS3O",
+  },
 ];
 
 const app = express();
@@ -24,31 +36,31 @@ app.post("/login", (req, res) => {
     (user) => user.email === email && user.password === password
   );
   if (!user) {
-    res.status(401).send("Invalid email or password");
+    res.status(401).send({ error: "Invalid email or password" });
     return;
   }
   user.password = undefined; // this removed password from the user object
   // now we want to sign/create a token...
   const token = jwt.sign(user, mySecretKey, { expiresIn: "1h" });
-  res.send(token); // now it will send back something like: rgSgniseg09NOF2sinv
+  res.send({ token }); // now it will send back something like: rgSgniseg09NOF2sinv
 });
 app.get("/public", (req, res) => {
-  res.send("welcome!");
+  res.send({ message: "welcome!" });
 });
 app.get("/private", (req, res) => {
   // lets require a valid token to see this
   const token = req.headers.authorization || "";
   if (!token) {
-    res.status(401).send("you must be logged in to see this");
+    res.status(401).send({ error: "you must be logged in to see this" });
     return;
   }
   jwt.verify(token, mySecretKey, (err, decoded) => {
     if (err) {
-      res.status(401).send("you must be logged in to see this" + err);
+      res.status(401).send({ error: "you must be logged in to see this" });
       return;
     }
     // here we know that the token is valid...
-    res.send(`Welcome ${decoded.email}`);
+    res.send({ message: `Welcome ${decoded.email}` });
   });
 });
 
